7be65c2e91288e7a7a70611e6bce6d3c507af0c0
ssk42 <sthreitz@gmail.com>
2023-08-29 09:02:54 -0400

    Initial Commit

---
+++ LangeForReddit/LangeForRedditApp.swift
@@ -0,0 +1,17 @@
+//
+//  LangeForRedditApp.swift
+//  LangeForReddit
+//
+//  Created by Stephen Reitz on 8/29/23.
+//
+
+import SwiftUI
+
+@main
+struct LangeForRedditApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
